2aef76f1f9d2683c41217a5a3eee479702ae80b7
Mario Danic <mario@lovelyhq.com>
2017-01-20 10:04:59 +0100

    Restore search state

--- src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
@@ -172,7 +172,10 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
         if (args.containsKey(FileDisplayActivity.EXTRA_SEARCH_QUERY)) {
             mSearchQuery = args.getString(FileDisplayActivity.EXTRA_SEARCH_QUERY);
         }
-        mSearchOpen = args.getBoolean(FileDisplayActivity.EXTRA_SEARCH, false);
+
+        if (args.getBoolean(FileDisplayActivity.EXTRA_SEARCH)) {
+            mSearchOpen = args.getBoolean(FileDisplayActivity.EXTRA_SEARCH, false);
+        }
 
         if (savedInstanceState == null) {
             if (file == null) {
@@ -210,7 +213,7 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
     }
 
 
-        private void loadAndShowTextPreview() {
+    private void loadAndShowTextPreview() {
         mTextLoadTask = new TextLoadAsyncTask(new WeakReference<>(mTextPreview));
         mTextLoadTask.execute(getFile().getStoragePath());
     }
@@ -218,6 +221,10 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
     @Override
     public boolean onQueryTextSubmit(String query) {
+        if (getActivity().getClass().equals(FileDisplayActivity.class)) {
+            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
+            fileDisplayActivity.setSearchQuery(query);
+        }
         mHandler.removeCallbacksAndMessages(null);
         if (query != null && !query.isEmpty()) {
             String coloredText = StringUtils.SearchAndColor(mOriginalText, query);
@@ -234,6 +241,10 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
     @Override
     public boolean onQueryTextChange(final String newText) {
+        if (getActivity().getClass().equals(FileDisplayActivity.class)) {
+            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) getActivity();
+            fileDisplayActivity.setSearchQuery(newText);
+        }
         mHandler.removeCallbacksAndMessages(null);
         mHandler.postDelayed(new Runnable() {
             @Override
@@ -320,7 +331,11 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
 
             if (textView != null) {
                 mOriginalText = new String(stringWriter.getBuffer());
+                mSearchView.setOnQueryTextListener(PreviewTextFragment.this);
                 textView.setText(mOriginalText);
+                if (mSearchQuery != null && mSearchOpen) {
+                    mSearchView.setQuery(mSearchQuery, true);
+                }
                 textView.setVisibility(View.VISIBLE);
             }
 
@@ -343,7 +358,14 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
         MenuItem menuItem = menu.findItem(R.id.action_search);
         menuItem.setVisible(true);
         mSearchView = (SearchView) MenuItemCompat.getActionView(menuItem);
-        mSearchView.setOnQueryTextListener(this);
+
+        if (mSearchOpen) {
+            if (mSearchQuery != null) {
+                mSearchView.setQuery(mSearchQuery, false);
+            }
+            mSearchView.setIconified(false);
+            mSearchView.clearFocus();
+        }
 
     }
 
