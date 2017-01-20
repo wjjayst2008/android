d6417275d30f6da4bae775b13b31ad5096a434e3
Mario Danic <mario@lovelyhq.com>
2017-01-20 10:26:34 +0100

    Fix codacy issues

--- src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
@@ -225,7 +225,7 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
         }
         mHandler.removeCallbacksAndMessages(null);
         if (query != null && !query.isEmpty()) {
-            String coloredText = StringUtils.SearchAndColor(mOriginalText, query);
+            String coloredText = StringUtils.searchAndColor(mOriginalText, query);
             mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
         } else {
             mTextPreview.setText(mOriginalText);
@@ -248,7 +248,7 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
             @Override
             public void run() {
                 if (newText != null && !newText.isEmpty()) {
-                    String coloredText = StringUtils.SearchAndColor(mOriginalText, newText);
+                    String coloredText = StringUtils.searchAndColor(mOriginalText, newText);
                     mTextPreview.setText(Html.fromHtml(coloredText.replace("\n", "<br \\>")));
                 } else {
                     mTextPreview.setText(mOriginalText);
